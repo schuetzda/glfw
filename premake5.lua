e3ef5504f2b423da0f93c960404616eff551d58a
schuetzda <david-schuetz@gmx.de>
2023-09-13 12:19:15 +0700

    Updated premake files

--- premake5.lua
+++ premake5.lua
@@ -15,8 +15,17 @@ project "GLFW"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
 	}
 	filter "system:linux"
 		pic "On"
@@ -26,16 +35,13 @@ project "GLFW"
 
 		files
 		{
+			"src/x11_platform.h",
+			"src/xkb_unicode.h",
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+			"src/glx_context.c"
 		}
 
 		defines
@@ -49,15 +55,13 @@ project "GLFW"
 
 		files
 		{
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
 			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/wgl_context.c"
 		}
 
 		defines 
